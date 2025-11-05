3cd96894f427ea33f3b73bdab586e50256eacbf9
S34nGS <steenhuis20@gmail.com>
2025-11-05 15:24:50 +0100

    fixed linting

--- player.py
+++ player.py
@@ -1,14 +1,14 @@
 class Player:
     def __init__(
-            self, 
-            health, 
+            self,
+            health,
             heavy_attack,
             light_attack,
             defense,
             heavy_attack_pp,
             light_attack_pp,
             inventory
-            ):
+    ):
         self.health = health
         self.heavy_attack = heavy_attack
         self.light_attack = light_attack
