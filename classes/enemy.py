0ab34e17ab240606cf5ee598fe53a7f9421d2a7f
Sean Steenhuis <sean@initworks.com>
2025-11-07 11:02:59 +0100

    added type hints

---
+++ classes/enemy.py
@@ -0,0 +1,6 @@
+class Enemy:
+    def __init__(self, health: int, heavy_attack: int, light_attack: int, defense: int):
+        self.health = health
+        self.heavy_attack = heavy_attack
+        self.light_attack = light_attack
+        self.defense = defense
